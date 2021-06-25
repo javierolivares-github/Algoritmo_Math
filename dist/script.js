7605016d2f752631e3cea1824a84b0f80e43f019
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-06-25 12:02:01 -0400

    Ajustes en rama-02

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,10 @@
 const form = document.getElementById('form');
 const input1 = document.getElementById('input1');
 const input2 = document.getElementById('input2');
+const suma = document.getElementById('suma');
+const producto = document.getElementById('producto');
+const división = document.getElementById('división');
+const promedio = document.getElementById('promedio');
 
 
 form.addEventListener('submit', (e) => {
@@ -15,15 +19,24 @@ form.addEventListener('submit', (e) => {
         const di = parseInt(input1.value) / parseInt(input2.value);
         const pro = su/2;
 
-        // Salida
+        // Salida consola
         console.log('La suma de tus números es: ' + su);
         console.log('La multiplicación de tus números es: ' + mu);
         console.log('La división de tus números es: ' + di);
         console.log('El promedio de tus números es: ' + pro);
+
+        // Salida pantalla
+        suma.innerText = `La suma de tus números es: ${su}`
+        producto.innerText = `La multiplicación de tus números es: ${mu}`
+        división.innerText = `La división de tus números es: ${di}`
+        promedio.innerText = `El promedio de tus números es: ${pro}`
+
     } else {
         // Salida
         console.log('Inserte los valores de n1 y n2.')
     }
 })
+
+// Fin
 
 
