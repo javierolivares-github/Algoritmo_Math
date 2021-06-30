f307dd02901aec2d50f2d848fb2fbf91912a54f6
Francisco Javier Olivares Diaz <franciscojavier.olivaresd@gmail.com>
2021-06-30 13:36:24 -0400

    avances 30062021

--- dist/script.js
+++ dist/script.js
@@ -12,30 +12,6 @@ let promedio = document.getElementById('promedio');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    // if (input1.value !== "" && input2.value !== "") {
-    //     // Proceso
-    //     const su = parseInt(input1.value) + parseInt(input2.value);
-    //     const mu = parseInt(input1.value) * parseInt(input2.value);
-    //     const di = parseInt(input1.value) / parseInt(input2.value);
-    //     const pro = su/2;
-
-    //     // Salida consola
-    //     console.log('La suma de tus números es: ' + su);
-    //     console.log('La multiplicación de tus números es: ' + mu);
-    //     console.log('La división de tus números es: ' + di);
-    //     console.log('El promedio de tus números es: ' + pro);
-
-    //     // Salida pantalla
-    //     suma.innerText = `La suma de tus números es: ${su}`
-    //     producto.innerText = `La multiplicación de tus números es: ${mu}`
-    //     división.innerText = `La división de tus números es: ${di.toFixed(2)}`
-    //     promedio.innerText = `El promedio de tus números es: ${pro.toFixed(0)}`
-
-    // } else {
-    //     // Salida
-    //     console.log('Inserte los valores de n1 y n2.')
-    // }
-
     if ((input1.value === "") || (input2.value === "")) {
         console.log('error: caracter vacio');
     } 
@@ -43,8 +19,6 @@ form.addEventListener('submit', (e) => {
         console.log('error: no es un número');
     } 
     else {
-        console.log('continuar');
-
         // Proceso
         let su = Number(input1.value) + Number(input2.value);
         let mu = Number(input1.value) * Number(input2.value);
@@ -62,13 +36,11 @@ form.addEventListener('submit', (e) => {
         console.log('El promedio de tus números es: ' + pro);
 
         // Salida pantalla
-        suma.innerText = `La suma de tus números es: ${su}`
-        producto.innerText = `La multiplicación de tus números es: ${mu.toFixed(1)}`
-        división.innerText = `La división de tus números es: ${di.toFixed(1)}`
-        promedio.innerText = `El promedio de tus números es: ${pro.toFixed(1)}`
-
+        suma.innerText = 'La suma de tus números es: ' + su;
+        producto.innerText = 'La multiplicación de tus números es: ' + mu;
+        división.innerText = 'La división de tus números es: ' + di;
+        promedio.innerText = 'El promedio de tus números es: ' + pro;
     };
-
 })
 
 // Fin
